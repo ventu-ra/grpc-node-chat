7a34aae18246b75ede13d4395e16bd3d1520589b
ventu-ra <ventura09.10.0@gmail.com>
2025-08-24 22:59:46 -0300

    Refactor server.js and update package.json
    - Updated the start script in package.json to run server.js without HTTP/2 exposure.
    - Simplified the listarUsuarios function by removing unnecessary else block.
    - Improved code readability by adding consistent spacing and formatting.
    - Ensured early return in listarUsuarios when no users are logged in.
    - Cleaned up the enviar_msg_Todos function for better clarity and structure.

--- client.js
+++ client.js
@@ -4,15 +4,10 @@ var readline = require("readline");
 const { exit } = require("process");
 
 var data = new Date();
-var dia     = data.getDate();
-var mes     = data.getMonth();
-var ano     = data.getFullYear();
-var hora    = data.getHours();  
-var min     = data.getMinutes();    
-var seg     = data.getSeconds();
+
 // Formata a data e a hora
-var str_data = dia + '/' + (mes+1) + '/' + ano;
-var str_hora = hora + ':' + min + ':' + seg;
+var str_data = data.toLocaleDateString();
+var str_hora = data.toLocaleTimeString();
 
 var LGerr = false;
 
@@ -36,7 +31,7 @@ const REMOTE_SERVER = "0.0.0.0:5001";
 
 let username;
 
-// Cria gRPC Cliente 
+// Cria gRPC Cliente
 let client = new proto.unesc.Chat(
   REMOTE_SERVER,
   grpc.credentials.createInsecure()
@@ -45,31 +40,28 @@ let client = new proto.unesc.Chat(
 
 // Inicializa a transmissão entre o servidor e o cliente
 function startChat() {
-  try 
-  {
+  try {
     let channel = client.entrar({ user: username });
-    
-    client.enviar({ user: username, text: "::. Entrou no servidor .::" }, res => {});
+
+    client.enviar({ user: username, text: "::. Entrou no servidor .::" }, res => { });
 
     channel.on("data", onData);
-    
-    rl.on("line", function(text) {
-        if(text == "")
-          return;
-        
-        if(text == "exit")
-        {
-          client.enviar({ user: username, text: "exit" }, res => {});
-          //exit();
-          //throw "Usuario vazou";
-        }
-        else
-          client.enviar({ user: username, text: text }, res => {});
+
+    rl.on("line", function (text) {
+      if (text == "")
+        return;
+
+      if (text == "exit") {
+        client.enviar({ user: username, text: "exit" }, res => { });
+        //exit();
+        //throw "Usuario vazou";
+      }
+      else
+        client.enviar({ user: username, text: text }, res => { });
     });
 
   }
-  catch (e) 
-  {
+  catch (e) {
     console.error(e);
   }
 
@@ -78,11 +70,9 @@ function startChat() {
 
 // Quando servidor envia uma mensagem
 function onData(message) {
-  
-  if (message.user == username) 
-  {
-    if(!LGerr)
-    {
+
+  if (message.user == username) {
+    if (!LGerr) {
       console.log("Olá " + username + " envie uma mensagem ...");
       LGerr = true;
       return;
@@ -98,16 +88,16 @@ function onData(message) {
 
 // Pergunta qual o nome do usuário quando inicializa o chat
 rl.question("Qual seu nome? ", answer => {
-  username = answer; 
+  username = answer;
 
   //verificaUsers(username);
-  
+
   startChat();
 });
 
 
 /* function verificaUsers(username)
 {
-  console.log(client); 
+  console.log(client);
   return;
 } */
--- server.js
+++ server.js
@@ -27,33 +27,27 @@ function entrar(call, callback) {
 
 function listarUsuarios(call, callback) {
 
-  if(usuarios.length == 0)
-  {
+  if (usuarios.length == 0) {
     callback(null, { error: 1, msg: "Não existem usuários logados" });
+    return
   }
-  else
-  {
-    callback(null, {
-      error: 0,
-      msg: "Success",
-      users: usuarios,
-      qtd : usuarios.length
-    });
-  }
-
+  callback(null, {
+    error: 0,
+    msg: "Success",
+    users: usuarios,
+    qtd: usuarios.length
+  });
 }
 
 // Receba mensagem do usuario cliente
-function enviar(call, callback) {  
+function enviar(call, callback) {
   enviar_msg_Todos(call.request);
-      
 }
 
 // Envia a mensagem para todos os usuarios conectados ao client
 function enviar_msg_Todos(message) {
   users.forEach(user => {
-    if(message.text == "exit")
-    {
+    if (message.text == "exit") {
       message.text = "::. " + message.user + " saiu do servidor.::";
       LGsair = true;
     }
@@ -61,23 +55,24 @@ function enviar_msg_Todos(message) {
     user.write(message);
   });
 
-  if(usuarios.includes(message.user))
-  {
+  if (usuarios.includes(message.user)) {
     LGinsere = false;
   }
   else
     LGinsere = true;
 
-  if(LGinsere)
+  if (LGinsere)
     usuarios.push(message.user);
-  
+
 }
 
 // Define o servidor com os métodos e inicia
-server.addService(proto.unesc.Chat.service, { entrar: entrar, enviar: enviar, listarUsuarios: listarUsuarios, getAllUsers: (call, callback) => {
-  console.log(usuarios);
-  callback(null, usuarios);
-} });
+server.addService(proto.unesc.Chat.service, {
+  entrar: entrar, enviar: enviar, listarUsuarios: listarUsuarios, getAllUsers: (call, callback) => {
+    console.log(usuarios);
+    callback(null, usuarios);
+  }
+});
 
 server.bind(SERVER_ADDRESS, grpc.ServerCredentials.createInsecure());
 
